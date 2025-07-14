4f303db2db11c6544183f5d449fbb3917d2d336a
bkase <brandernan@gmail.com>
2025-07-14 22:50:08 +0100

    Disable smooth animations

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable smooth scrolling animation
+vim.g.snacks_scroll = false
